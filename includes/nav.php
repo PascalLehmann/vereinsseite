1293e2b30d93e3b27c86aef804a58a76e1deb693
Pascal Lehmann <pascallehmann1983@gmail.com>
2026-04-11 01:07:41 +0200

    Neue Ordnerstruktur im admin bereich

--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,4 @@
 <?php
-// Session nur starten, wenn sie nicht bereits läuft
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
@@ -21,15 +20,14 @@ if (session_status() === PHP_SESSION_NONE) {
                 <li>
                     <input type="checkbox" id="drop-mitglieder" class="drop-check">
                     <label for="drop-mitglieder" class="drop-label">
-                        <i class="fa-solid fa-users"></i> Mitglieder
-                        <i class="fa-solid fa-chevron-down" style="font-size: 0.8rem; margin-left: auto;"></i>
+                        <i class="fa-solid fa-users"></i> Mitglieder <i class="fa-solid fa-chevron-down"
+                            style="font-size: 0.8rem; margin-left: auto;"></i>
                     </label>
                     <ul class="sub-menu">
                         <li><a href="/vorstand.php"><i class="fa-solid fa-user-tie"></i> Vorstand</a></li>
                         <li><a href="/spieler.php"><i class="fa-solid fa-user-group"></i> Spieler</a></li>
                     </ul>
                 </li>
-
                 <li><a href="/galerie.php"><i class="fa-solid fa-images"></i> Galerie</a></li>
             </ul>
         </div>
@@ -38,19 +36,12 @@ if (session_status() === PHP_SESSION_NONE) {
             <?php if (isset($_SESSION['admin_id'])): ?>
                 <h3>Verwaltung</h3>
                 <ul>
-                    <li><a href="/admin/dashboard.php"><i class="fa-solid fa-gauge"></i> Dashboard</a></li>
-                    <li><a href="/admin/news/uebersicht.php"><i class="fa-solid fa-pen-to-square"></i> News verwalten</a>
-                    </li>
-                    <li><a href="/admin/mitglieder/uebersicht.php"><i class="fa-solid fa-user-gear"></i> Mitglieder
-                            verwalten</a></li>
-                    <li><a href="/admin/gegner/uebersicht.php"><i class="fa-solid fa-shield-halved"></i> Gegner
-                            verwalten</a></li>
-                    <li><a href="/admin/logout.php" class="logout-link"><i class="fa-solid fa-right-from-bracket"></i>
-                            Logout</a></li>
+                    <li><a href="/admin/dashboard.php">Dashboard</a></li>
+                    <li><a href="/admin/logout.php" class="logout-link">Logout</a></li>
                 </ul>
             <?php else: ?>
                 <ul>
-                    <li><a href="/admin/login.php"><i class="fa-solid fa-lock"></i> Admin Login</a></li>
+                    <li><a href="/admin/login.php">Admin Login</a></li>
                 </ul>
             <?php endif; ?>
         </div>
